7b149d1b6e420c51a86442103194c834d1663668
Jose Folgar <71222862+JoseMario3@users.noreply.github.com>
2026-05-21 09:41:54 -0400

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.js
@@ -0,0 +1,50 @@
+class Solution {
+
+    groupAnagrams(strs) {
+        const map = {};
+        for (const word of strs) {
+            const key = word.split('').sort().join('');
+            map[key] = map[key] || [];
+            map[key].push(word);
+        }
+        return Object.values(map);
+    }
+
+    // groupAnagrams(strs) {
+    //     let res = [], covered = [];
+    //     if (strs == null) return res;
+    //     for (let i = 0; i < strs.length; i++) {
+    //         let curr = [], map1 = {};
+
+    //         if (covered.includes(i)) continue;
+
+    //         let word = strs[i];
+    //         curr.push(strs[i]);
+    //         for (let k = 0; k < word.length; k++) {
+    //             map1[word[k]] = map1[word[k]] + 1 || 1;
+    //         }
+
+    //         for (let j = i + 1; j < strs.length; j++) {
+    //             let map2 = {};
+    //             for (let l = 0; l < strs[j].length; l++) {
+    //                 map2[strs[j][l]] = map2[strs[j][l]] + 1 || 1;
+    //             }
+    //             let same = true;
+
+    //             if (strs[j].length !== word.length) {
+    //                 same = false;
+    //             } else {
+    //                 for (const [key, value] of Object.entries(map1)) {
+    //                     if (value != map2[key]) {
+    //                         same = false;
+    //                         break;
+    //                     }
+    //                 }
+    //             }
+    //             if (same) covered.push(j), curr.push(strs[j]);
+    //         }
+    //         res.push(curr);
+    //     }
+    //     return res
+    // }
+}
