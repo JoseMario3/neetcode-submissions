2bb17219d194315cb6baef30b16536a138615fca
Jose Folgar <71222862+JoseMario3@users.noreply.github.com>
2026-06-01 15:51:23 -0400

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost) {
+        const n = cost.length;
+        if (n == 1) return cost[0];
+        if (n == 2) return Math.min(cost[0], cost[1]);
+        const dp = new Array(n).fill(0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (let i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i];
+        }
+        return Math.min(dp[n-2], dp[n-1]);
+    }
+}
